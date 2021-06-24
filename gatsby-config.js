c0724e5b281d02bf5376d7c3e0bde8a50bc34882
borgellaj <borgella@gmail.com>
2021-06-24 18:30:49 +0800

    netlify function

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,20 @@
+const createProxyMiddleware = require('http-proxy-middleware');
+
+
 module.exports = {
+    // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      createProxyMiddleware({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
   siteMetadata: {
     title: "portfoliofly",
   },
