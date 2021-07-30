5308b978d4161156dcd3417cf65aad1eebf0b8d5
borgellaj <borgella@gmail.com>
2021-07-29 17:38:08 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -1,57 +1,124 @@
-import { Link } from 'gatsby'
-import React from 'react'
+import { Link } from 'gatsby';
+import React from 'react';
 
-import Layout from "../components/layout"
-import Both from "../images/svg/both.svg"
-const Description  = (location) => {
-    return (
-        <Layout location={location}>
+import Layout from '../components/layout';
+import Both from '../images/svg/both.svg';
+const Description = (location) => {
+  return (
+    <Layout location={location}>
+      <div className='description'>
+        <h1>CWA/ACCA – JATC</h1>
+        <h1>CWA DISTICT 9 Telecommunications Apprenticeship Program</h1>
+        <h1>
+          <em>Related and Supplemental Instruction Courses</em>
+        </h1>
+        <div className='box'>
+          <h2>Installation Skills 2 (Structured Cabling)</h2>
+          <br />
+          <p>
+            Exposure to procedures and practices routinely used in the industry
+            of installing telecommunication cabling and equipment, including
+            EIA/TIA standards, applicable National Electrical Codes (NEC), IEEE
+            codes and standards for the telecommunications industry. Coverage of
+            health and safety issues and procedures. Overview and direct use of
+            appropriate tools, equipment and documentation required to install
+            and maintain communications systems. Demonstrate proficiency in
+            termination and testing of copper cable in accordance to the EIA/TIA
+            568 and 569 standards. Reading and interpreting layout and
+            distribution frames and wiring. Procedures for installing cable
+            racks and related equipment.
+          </p>
+          <br />
+          <p>
+            (Now offers an optional FOA CPCT certification at end of course.)
+          </p>
+          <br />
+          <h2>Fiber Installation and Maintenance </h2>
+          <br />
+          <p>
+            Introduction to Fiber Optic Technology. Provides coverage of theory,
+            installation procedures; testing procedures; proper maintenance and
+            trouble analysis of fiber optic systems. Introduction to multimode
+            and single mode fiber. Coverage of how generic fiber optic systems
+            operate. Procedures for interpreting the output of fiber optic
+            monitor controls, optical time domain reflectometers and error rate
+            test sets. Hands on exercises with fiber connectors, mechanical and
+            fusion splices.{' '}
+          </p>
+          <br />
+          <p>
+            (Now offers an optional FOA CFOT certification at end of course.){' '}
+          </p>
+          <br />
+          <h2>Health and Safety / OSHA</h2>
+          <br />
+          <p>
+            Coverage of health and safety issues and procedures in the
+            construction industry. Included in the course will be an
+            introduction to OSHA, Managing safety and health, falls,
+            electrocution, struck-by ( e.g., falling objects, trucks, cranes),
+            caught-in or between (e.g., trench hazards, equipment), personal
+            protective and lifesaving equipment, health hazards in construction,
+            stairways and ladders, confined space entry, Lockout/tagout, CPR and
+            first aid training, overall and direct use of appropriate tools and
+            equipment required to install and maintain communications systems.
+          </p>
+          <br />
+          <h2>Computer Literacy</h2>
+          <br />
+          <p>
+            Introduction to computers including: What is a computer, What’s
+            inside a computer, lap top and desktop computers, Hardware,
+            Software, Microsoft Windows, Internet, World Wide Web, Multimedia,
+            browsers, Microsoft Word, Microsoft Excel, and an awareness of
+            computer software in use including programming languages, electronic
+            mail, storage devices, printing. No prior computer experience
+            necessary; course recommended for students of any major who want to
+            learn about computers and how to use them.
+          </p>
+          <br />
+          <h2>Installation Skills 3 / Electronic Safety and Security</h2>
+          <br />
+          <p>
+            Introduction to the electronic safety and security provides coverage
+            of theory, devices, equipment, and installation procedures. The
+            course discusses principles of security, intrusion detection
+            systems, exploring the basics of design, and provides examples of
+            the application of various types of sensors used to provide
+            intrusion detection, various types of access control systems, video
+            surveillance fundamentals, notification display devices, special
+            systems such as nurse call, codes, standards, regulations, and
+            organizations.{' '}
+          </p>
+          <br />
+          <h2>INTRODUCTION TO COMPUTER NETWORKING</h2>
+          <br />
+          <p>
+            The Course provides a thorough overview of networking basics: MAC
+            and IP addressing, hubs, and switches, packets and ports, and OSI
+            versus TCP/IP models, cabling. Topologies and Ethernet basics,
+            basics of TCP/IP and an overview of routing, major TCP/IP
+            applications, http, https protocols, ports and network naming
+            conventions. This course also discusses VLAN, VPNs, IPv6 and remote
+            connectivity. Using wired (Ethernet) and wireless (Wi-Fi) methods to
+            connect the computers and equipment in a network. Sharing files
+            between computers and explaining how to manage connections to those
+            services with DHCP and DNS addressing. Setting up an internet
+            connection and configuring security and local-storage options for
+            the network.
+          </p>
+          <br />
+          <p>Apprenticeship Term: 36 months</p>
+          <p>6 courses (no particular sequence)</p>
+          <p>Semester basis (1 course per semester)</p>
+          <p>Each course is approximately 54 hours (108 hours per year)</p>
+          <div className='application'>
+            <Link to='/application'> Application</Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-            <div className="description">
-                <h1>CWA/ACCA – JATC</h1>
-                <h1>CWA DISTICT 9 Telecommunications Apprenticeship Program</h1>
-                <h1><em>Related and Supplemental Instruction Courses<em></h1>
-                <div className="box">
-                    <h2>Installation Skills 2 (Structured Cabling)</h2> 
-<br>
-                      <p>Exposure to procedures and practices routinely used in the industry of installing telecommunication cabling and equipment, including EIA/TIA standards, applicable National Electrical Codes (NEC), IEEE codes and standards for the telecommunications industry.  Coverage of health and safety issues and procedures.  Overview and direct use of appropriate tools, equipment and documentation required to install and maintain communications systems.  Demonstrate proficiency in termination and testing of copper cable in accordance to the EIA/TIA 568 and 569 standards.  Reading and interpreting layout and distribution frames and wiring.  Procedures for installing cable racks and related equipment.</p>
-                  <br>
-                        <p>(Now offers an optional FOA CPCT certification at end of course.)</p>
-                  <br>
-                        <h2>Fiber Installation and Maintenance </h2>
-                  <br>
-                      <p>Introduction to Fiber Optic Technology.  Provides coverage of theory, installation procedures; testing procedures; proper maintenance and trouble analysis of fiber optic systems.  Introduction to multimode and single mode fiber.  Coverage of how generic fiber optic systems operate.  Procedures for interpreting the output of fiber optic monitor controls, optical time domain reflectometers and error rate test sets.  Hands on exercises with fiber connectors, mechanical and fusion splices. </p>    
-                  <br>
-                        <p>(Now offers an optional FOA CFOT certification at end of course.) </p>
-                  <br>
-                        <h2>Health and Safety / OSHA</h2>
-                  <br>
-                          <p>Coverage of health and safety issues and procedures in the construction industry.  Included in the course will be an introduction to OSHA, Managing safety and health, falls, electrocution, struck-by ( e.g., falling objects, trucks, cranes), caught-in or between (e.g., trench hazards, equipment), personal protective and lifesaving equipment, health hazards in construction, stairways and ladders, confined space entry, Lockout/tagout, CPR and first aid training, overall and direct use of appropriate tools and equipment required to install and maintain communications systems.</p>
-                  <br>
-                            <h2>Computer Literacy</h2>
-                  <br>
-                              <p>Introduction to computers including:  What is a computer, What’s inside a computer, lap top and desktop computers, Hardware, Software, Microsoft Windows, Internet, World Wide Web, Multimedia, browsers, Microsoft Word, Microsoft Excel, and an awareness of computer software in use including programming languages, electronic mail, storage devices, printing.  No prior computer experience necessary; course recommended for students of any major who want to learn about computers and how to use them.</p>
-                  <br>
-                                <h2>Installation Skills 3 / Electronic Safety and Security</h2>
-                  <br>
-                                  <p>Introduction to the electronic safety and security provides coverage of theory, devices, equipment, and installation procedures.  The course discusses principles of security, intrusion detection systems, exploring the basics of design, and provides examples of the application of various types of sensors used to provide intrusion detection, various types of access control systems, video surveillance fundamentals, notification display devices, special systems such as nurse call, codes, standards, regulations, and organizations.  </p>
-                  <br>
-                                    <h2>INTRODUCTION TO COMPUTER NETWORKING</h2>
-                  <br>
-                                      <p>The Course provides a thorough overview of networking basics:  MAC and IP addressing, hubs, and switches, packets and ports, and OSI versus TCP/IP models, cabling.  Topologies and Ethernet basics, basics of TCP/IP and an overview of routing, major TCP/IP applications, http, https protocols, ports and network naming conventions.  This course also discusses VLAN, VPNs, IPv6 and remote connectivity.  Using wired (Ethernet) and wireless (Wi-Fi) methods to connect the computers and equipment in a network.  Sharing files between computers and explaining how to manage connections to those services with DHCP and DNS addressing.  Setting up an internet connection and configuring security and local-storage options for the network.</p>
-<br>
-                                        <p>Apprenticeship Term: 36 months</p>
-                                        <p>6 courses (no particular sequence)</p>
-                                        <p>Semester basis (1 course per semester)</p>
-                                        <p>Each course is approximately 54 hours (108 hours per year)</p>
-                        <div className="application" ><Link to="/application" > Application</Link></div>
-                    </div>
-
-                </div>
-
-            </div>
-        </Layout>
-    )
-}
-
-export default Description 
+export default Description;
